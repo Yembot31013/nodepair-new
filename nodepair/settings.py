49d3a8e8e0d89d45e44b0723540058f72b25e3bf
Yembot31013 <yembot31013@gmail.com>
2023-12-04 20:13:19 +0200

    modified settings.py file

--- nodepair/settings.py
+++ nodepair/settings.py
@@ -172,7 +172,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Turn on WhiteNoise storage backend that takes care of compressing static files
 # and creating unique names for each version so they can safely be cached forever.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
